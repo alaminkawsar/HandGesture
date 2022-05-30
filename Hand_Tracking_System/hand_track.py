6bf69fcf6de21e9821cad352537d912427b8746a
alaminkawsar <alaminkawsar8643@gmail.com>
2022-05-30 18:29:08 +0600

    main python script

--- Hand_Tracking_System/hand_track.py
+++ Hand_Tracking_System/hand_track.py
@@ -1,41 +1,79 @@
-import cv2
-import mediapipe as mp
-import time
+# TechVidvan hand Gesture Recognizer
 
-cap = cv2.VideoCapture(0)
+# import necessary packages
+
+import cv2
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+# initialize mediapipe
 mpHands = mp.solutions.hands
-hands = mpHands.Hands(static_image_mode=False,
-                      max_num_hands=4,
-                      min_detection_confidence=0.5,
-                      min_tracking_confidence=0.5)
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
 mpDraw = mp.solutions.drawing_utils
 
-pTime = 0
-cTime = 0
+# Load the gesture recognizer model
+model = load_model('mp_hand_gesture')
+
+# Load class names
+f = open('gesture.names', 'r')
+classNames = f.read().split('\n')
+f.close()
+print(classNames)
+
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
 
 while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    #print(success, results.multi_hand_landmarks)
+    # Read each frame from the webcam
+    _, frame = cap.read()
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x *w), int(lm.y*h)
-                #if id ==0:
-                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+    x, y, c = frame.shape
 
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    # Flip the frame vertically
+    frame = cv2.flip(frame, 1)
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    # Get hand landmark prediction
+    result = hands.process(framergb)
 
-    cTime = time.time()
-    fps = 1/(cTime-pTime)
-    pTime = cTime
+    # print(result)
+    
+    className = ''
 
-    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+    # post process the result
+    if result.multi_hand_landmarks:
+        landmarks = []
+        for handslms in result.multi_hand_landmarks:
+            for lm in handslms.landmark:
+                # print(id, lm)
+                lmx = int(lm.x * x)
+                lmy = int(lm.y * y)
 
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+                landmarks.append([lmx, lmy])
+
+            # Drawing landmarks on frames
+            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+
+            # Predict gesture
+            prediction = model.predict([landmarks])
+            # print(prediction)
+            classID = np.argmax(prediction)
+            className = classNames[classID]
+
+    # show the prediction on the frame
+    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+                   1, (0,0,255), 2, cv2.LINE_AA)
+
+    # Show the final output
+    cv2.imshow("Output", frame) 
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+# release the webcam and destroy all active windows
+cap.release()
+
+cv2.destroyAllWindows()
